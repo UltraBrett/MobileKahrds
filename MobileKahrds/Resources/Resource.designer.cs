ed27532793e1e738b55539c6c621d81163c69e9f
UltraBrett <brett.stanaland@geofeedia.com>
2015-03-02 16:19:52 -0500

    Buttons correctly placed on main page
    also added appropriate extensions to gitignore

--- MobileKahrds/Resources/Resource.designer.cs
+++ MobileKahrds/Resources/Resource.designer.cs
@@ -61,7 +61,7 @@ namespace MobileKahrds
 		{
 			
 			// aapt resource value: 0x7f050000
-			public const int myButton = 2131034112;
+			public const int layoutContainer = 2131034112;
 			
 			static Id()
 			{
